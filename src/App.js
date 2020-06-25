d3d035df262cb3ea822abcd1d8a6e5ca65263278
dacardenasa <diego.cardenasaleg@gmail.com>
2020-06-24 19:21:59 -0500

    Add function showTest to component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const paragraph = <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
 class App extends Component {
+
+  constructor(){
+    super();
+    this.showText = this.showText.bind(this);
+
+    this.state = {
+      terms: false
+    }
+    
+  }
+
   render() {
     return (
       <div className="wrapper">
-        <label><input type="checkbox" /> Mostrar información importante</label>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <label>
+          <input type="checkbox" onClick={this.showText} /> Mostrar información importante
+          { this.state.terms ? paragraph : null }
+        </label>
       </div>
+      
     );
+  }
+
+  showText(e){
+    this.setState({
+      terms: !this.state.terms
+    });
   }
 }
 
